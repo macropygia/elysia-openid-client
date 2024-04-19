a03efdfcea62bca986ce46d9b9e0bb7b06820b1f
macropygia <3162324+macropygia@users.noreply.github.com>
2024-04-20 02:47:56 +0900

    test: add test to deleteCookie

---
+++ core/deleteCookie.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, mock, test } from "bun:test";
+import { type DeepPartial, baseMockClient, mockCookie } from "@/__test__/const";
+import type { OidcClient } from "@/core/OidcClient";
+import { defaultCookieSettings } from "./const";
+import { deleteCookie } from "./deleteCookie";
+
+describe("Unit/core/deleteCookie", () => {
+  test("Default", () => {
+    const ms = {
+      ...baseMockClient,
+      logger: {
+        trace: mock(),
+        debug: mock(),
+      },
+    } as DeepPartial<OidcClient> as OidcClient;
+
+    deleteCookie(ms, mockCookie);
+
+    expect(ms.logger?.trace).toHaveBeenCalled();
+    expect(ms.logger?.debug).toHaveBeenCalled();
+    expect(
+      mockCookie[defaultCookieSettings.sessionIdName].update,
+    ).toHaveBeenCalled();
+  });
+});
