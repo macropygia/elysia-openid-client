aca4a250964229f5cea30c36b83d536079e869a4
macropygia <3162324+macropygia@users.noreply.github.com>
2024-04-21 00:16:19 +0900

    fix(logger): fix wrong operator in consoleLogger

---
+++ loggers/consoleLogger.test.ts
@@ -0,0 +1,107 @@
+import {
+  type Mock,
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  spyOn,
+  test,
+} from "bun:test";
+import { consoleLogger } from "./consoleLogger";
+
+describe("consoleLogger", () => {
+  let logDebug: Mock<() => void>;
+  let logInfo: Mock<() => void>;
+  let logWarn: Mock<() => void>;
+  let logError: Mock<() => void>;
+
+  beforeEach(() => {
+    logDebug = spyOn(console, "debug").mockImplementation(() => undefined);
+    logInfo = spyOn(console, "info").mockImplementation(() => undefined);
+    logWarn = spyOn(console, "warn").mockImplementation(() => undefined);
+    logError = spyOn(console, "error").mockImplementation(() => undefined);
+  });
+
+  afterEach(() => {
+    logDebug.mockRestore();
+    logInfo.mockRestore();
+    logWarn.mockRestore();
+    logError.mockRestore();
+  });
+
+  test("default", () => {
+    const logger = consoleLogger();
+
+    logger.silent("test"); // Not called
+    logger.trace("test"); // Called
+    logger.debug("test"); // Called
+    expect(logDebug).toHaveBeenCalledTimes(2);
+
+    logger.info("test"); // Called
+    expect(logInfo).toHaveBeenCalledTimes(1);
+
+    logger.warn("test"); // Called
+    expect(logWarn).toHaveBeenCalledTimes(1);
+
+    logger.error("test"); // Called
+    logger.fatal("test"); // Called
+    expect(logError).toHaveBeenCalledTimes(2);
+  });
+
+  test("info", () => {
+    const logger = consoleLogger("info");
+
+    logger.silent("test"); // Not called
+    logger.trace("test"); // Not called
+    logger.debug("test"); // Not called
+    expect(logDebug).toHaveBeenCalledTimes(0);
+
+    logger.info("test"); // Called
+    expect(logInfo).toHaveBeenCalledTimes(1);
+
+    logger.warn("test"); // Called
+    expect(logWarn).toHaveBeenCalledTimes(1);
+
+    logger.error("test"); // Called
+    logger.fatal("test"); // Called
+    expect(logError).toHaveBeenCalledTimes(2);
+  });
+
+  test("silent", () => {
+    const logger = consoleLogger("silent");
+
+    logger.silent("test"); // Called
+    logger.trace("test"); // Called
+    logger.debug("test"); // Called
+    expect(logDebug).toHaveBeenCalledTimes(3);
+
+    logger.info("test"); // Called
+    expect(logInfo).toHaveBeenCalledTimes(1);
+
+    logger.warn("test"); // Called
+    expect(logWarn).toHaveBeenCalledTimes(1);
+
+    logger.error("test"); // Called
+    logger.fatal("test"); // Called
+    expect(logError).toHaveBeenCalledTimes(2);
+  });
+
+  test("error", () => {
+    const logger = consoleLogger("error");
+
+    logger.silent("test"); // Not called
+    logger.trace("test"); // Not called
+    logger.debug("test"); // Not called
+    expect(logDebug).toHaveBeenCalledTimes(0);
+
+    logger.info("test"); // Not called
+    expect(logInfo).toHaveBeenCalledTimes(0);
+
+    logger.warn("test"); // Not called
+    expect(logWarn).toHaveBeenCalledTimes(0);
+
+    logger.error("test"); // Called
+    logger.fatal("test"); // Called
+    expect(logError).toHaveBeenCalledTimes(2);
+  });
+});
--- loggers/consoleLogger.ts
+++ loggers/consoleLogger.ts
@@ -9,20 +9,20 @@ import type { LevelWithSilent } from "pino";
 export const consoleLogger = (
   level?: keyof typeof levels,
 ): OIDCClientLogger => ({
-  silent: levels[level || "trace"] >= 99 ? console.debug : () => undefined,
-  trace: levels[level || "trace"] >= 10 ? console.debug : () => undefined,
-  debug: levels[level || "trace"] >= 20 ? console.debug : () => undefined,
-  info: levels[level || "trace"] >= 30 ? console.info : () => undefined,
-  warn: levels[level || "trace"] >= 40 ? console.warn : () => undefined,
-  error: levels[level || "trace"] >= 50 ? console.error : () => undefined,
-  fatal: levels[level || "trace"] >= 60 ? console.error : () => undefined,
+  silent: levels[level || "trace"] === 0 ? console.debug : () => undefined,
+  trace: levels[level || "trace"] <= 10 ? console.debug : () => undefined,
+  debug: levels[level || "trace"] <= 20 ? console.debug : () => undefined,
+  info: levels[level || "trace"] <= 30 ? console.info : () => undefined,
+  warn: levels[level || "trace"] <= 40 ? console.warn : () => undefined,
+  error: levels[level || "trace"] <= 50 ? console.error : () => undefined,
+  fatal: levels[level || "trace"] <= 60 ? console.error : () => undefined,
 });
 
 const levels: Record<LevelWithSilent, number> = {
-  silent: 99,
+  silent: 0,
   fatal: 60,
   error: 50,
-  warn: 50,
+  warn: 40,
   info: 30,
   debug: 20,
   trace: 10,
